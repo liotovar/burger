cbacfef76d68f395720f061f440309dc27791dc8
liotovar <tovar.lionel@gmail.com>
2017-12-06 21:30:01 -0600

    files added

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("CheeseBurger", true);
+INSERT INTO burgers (burger_name) VALUES ("Hamburger");
+
